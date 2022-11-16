f807c532e31f8ff2e5dee7e002ce72cb77d497ce
crenaz <crenaz@gmail.com>
2022-11-16 01:14:43 -0400

    114

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import { SITE } from './src/config';
-import sitemap from '@astrojs/sitemap';
+
 import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), sitemap()],
+  integrations: [mdx()],
   server: { host: '0.0.0.0' },
   site: SITE.url,
   output: 'server',
