cb6534f6e5a986e2549e78ea88d905fb13bef986
crenaz <crenaz@gmail.com>
2023-12-11 18:56:57 -0500

    new page, who dis?

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,4 @@
 import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import { SITE } from './src/config';
-
-import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
-export default defineConfig({
-  integrations: [mdx()],
-  server: { host: '0.0.0.0' },
-  site: SITE.url,
-  output: 'server',
-  adapter: cloudflare(),
-});
+export default defineConfig({});
