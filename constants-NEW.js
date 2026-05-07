87f08b4c98d5eb4c7931c1432f33e85a9f29aacb
crenaz <crenaz@gmail.com>
2026-05-07 18:55:50 -0400

    constants from list to semantic desc

---
+++ constants-NEW.js
@@ -0,0 +1,38 @@
+export const images = [
+  {
+    slug: "viejo-san-juan",
+    title: "Viejo San Juan",
+    unslashId: "photo-1653511871201-83252ff23e90",
+    description: "Calle de O'Donnell",
+  },
+  {
+    slug: "new-orleans",
+    title: "New Orleans",
+    unslashId: "photo-1678839041538-f5b45013e377",
+    description: "St Louis St.",
+  },
+  {
+    slug: "brooklyn",
+    title: "Brooklyn Bridge",
+    unslashId: "photo-1585163435462-7e7796fa4b9e",
+    description: "A view from Brooklyn",
+  },
+  {
+    slug: "manhattan",
+    title: "Manhattan - NYC",
+    unslashId: "photo-1571101084667-5f37d7954316",
+    description: "Somewhere in SOHO",
+  },
+  {
+    slug: "luquillo",
+    title: "Playa de Luquillo",
+    unslashId: "photo-1625359962064-be18caa4e97b",
+    description: "Luquillo, Puerto Rico",
+  },
+  {
+    slug: "rincon",
+    title: "Playa de Rincon",
+    unslashId: "photo-1509233725247-49e657c54213",
+    description: "Rincon, Puerto Rico",
+  },
+];
