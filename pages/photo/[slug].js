e1ad2faea0202b5e9979e7a793698d9444cc9fd2
crenaz <crenaz@gmail.com>
2026-05-07 20:55:40 -0400

    tryin' to refactor but it broke

---
+++ pages/photo/[slug].js
@@ -0,0 +1,30 @@
+import * as React from "react";
+import SingleImage from "../../components/SingleImage";
+import { images } from "../../constants";
+
+const Page = ({ image }) => {
+  return <SingleImage iamge={image} />;
+};
+
+export async function getStaticProps({ params }) {
+  const image = images.find((img) => img.slug === params.slug);
+
+  return {
+    props: {
+      image,
+    },
+  };
+}
+
+export async function getStaticPaths() {
+  return {
+    paths: images.map((image) => ({
+        params: {
+          slug: image.slug,
+        },
+    })),
+    fallback: false,
+  };
+}
+
+export default Page;
